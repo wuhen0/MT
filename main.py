b5c1549d484dbafc7d941f81509662b073fb470d
FlexHook <132535742+FlexHook@users.noreply.github.com>
2025-11-28 22:58:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,43 +12,12 @@ MAX_RETRY = 3
 Retry = {}
 accounts_list = {}
 
-
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
     'Connection': 'keep-alive'
 }
-
-s.add('111.22.7.195:8008')
-s.add('121.237.181.137:8888')
-s.add('36.136.27.2:4999')
-s.add('42.180.0.58:6182')
-s.add('36.110.143.55:8080')
-s.add('221.217.54.145:9000')
-s.add('123.128.12.93:9050')
-s.add('220.197.44.36:3128')
-s.add('120.92.211.211:7890')
-s.add('222.129.137.95:9000')
-s.add('112.111.13.253:7890')
-s.add('14.103.233.245:3128')
-s.add('61.169.156.182:8118')
-s.add('222.59.173.105:45167')
-s.add('221.226.132.162:10081')
-s.add('58.249.55.222:9797')
-s.add('36.147.78.166:80')
-s.add('117.186.78.226:18080')
-s.add('1.85.33.94:6666')
-s.add('210.75.240.135:10044')
-s.add('202.112.51.124:3128')
-s.add('222.59.173.105:45123')
-s.add('39.185.41.193:5911')
-s.add('1.94.145.211:8989')
-s.add('120.92.212.16:7890')
-s.add('139.159.97.42:9798')
-s.add('36.129.129.215:9000')
-s.add('123.112.244.220:9000')
 
 def verify():
     global ip
@@ -60,7 +29,7 @@ def verify():
         if not s:
             break
         proxy = random.choice(list(s))
-        print(f"{attempt_count}: {proxy}", end="\r")
+        print(f"{attempt_count}: {proxy}")
         proxies = {
             'https': f'http://{proxy}',
             'http': f'http://{proxy}'
@@ -68,14 +37,14 @@ def verify():
         try:
             response = requests.get(target_url, headers=headers, proxies=proxies, timeout=10)
             if response.status_code == 200:
-                print(f"{attempt_count}: {proxy}", "✔")
+                print("✔")
                 ip = proxy
                 s.remove(proxy)
                 return
         except Exception as e:
             pass
         s.remove(proxy)
-        print(f"{attempt_count}: {proxy}", "✖")
+        print("✖")
         time.sleep(0.1)
 
 def checkIn(user, pwd):
@@ -167,7 +136,7 @@ def start():
                 time.sleep(3)
         start()
 
-if 'mtluntan' in os.environ:
+if 'mtluntan' in os.environ and 'ips' in os.environ:
     fen = os.environ.get("mtluntan", "").split(",")
     for duo in fen:
         if ':' not in duo:
@@ -177,6 +146,7 @@ if 'mtluntan' in os.environ:
         password = password.strip()
         if username and password:
             accounts_list[username] = password
+    s.update(os.environ.get("ips", "").split("\n"))
     start()
 else:
     print('github变量未设置')    
